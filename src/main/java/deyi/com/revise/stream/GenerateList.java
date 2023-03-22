a62780921f554211f57d4eb1d421d12ed6d5422e
Deyi3417 <18811553417@163.com>
2023-03-22 14:28:04 +0800

    每日小技巧

---
+++ src/main/java/deyi/com/revise/stream/GenerateList.java
@@ -0,0 +1,9 @@
+package deyi.com.revise.stream;
+
+/**
+ * @author : HP
+ * @date : 2023/3/22
+ */
+public class GenerateList {
+
+}
--- src/main/java/deyi/com/threadpool/TestThreadPool.java
+++ src/main/java/deyi/com/threadpool/TestThreadPool.java
@@ -10,13 +10,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestThreadPool {
     public static void main(String[] args) {
-        // 创建线程池
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 4, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(10));
+        // 创建线程池 -优势：性能 没有一直创建和销毁的过程，性能好
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 21, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(20));
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 21, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(20));
+        long startTime = System.currentTimeMillis();
         // 提交任务
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             executor.execute(new Task(i));
+            System.out.println("are you ok");
         }
-        // 关闭线程池
+        long endTime = System.currentTimeMillis();
+        System.out.println("耗时：" + (endTime - startTime));
+        // 关闭线程池 等待子线程执行完 waitmodify socket()
         executor.shutdown();
+
+        System.out.println("线程shutDown之后：" + (System.currentTimeMillis() - startTime));
     }
 }
