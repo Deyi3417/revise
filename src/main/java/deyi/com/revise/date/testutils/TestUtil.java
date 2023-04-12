3c16b7b54db17d541d7b1cb4ae1e1547dd21ab2b
Deyi3417 <18811553417@163.com>
2023-04-12 14:14:25 +0800

    日期计算，往前顺序推几天

--- src/main/java/deyi/com/revise/date/testutils/TestUtil.java
+++ src/main/java/deyi/com/revise/date/testutils/TestUtil.java
@@ -2,6 +2,9 @@ package deyi.com.revise.date.testutils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
@@ -9,10 +12,43 @@ import java.util.Date;
  * @Create 2022/2/7 15:53
  */
 public class TestUtil {
-    public static void main(String[] args) throws ParseException {
-//        List<String> list = DateTimeUtil.MothDay();
-//        System.out.println("list:" + list);
-        getTime();
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String format = today.format(formatter);
+        System.out.println(format);
+        for (int i = 1; i <= 10; i++) {
+            LocalDate newDate = today.minusDays(i);
+            String formattedDate = newDate.format(formatter);
+            System.out.println("Date " + i + " day(s) ago: " + formattedDate);
+        }
+
+//
+//        // 输入的日期字符串
+//        String dateString = "2022-02-22";
+//        // 解析日期字符串
+//        LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//        for (int i = 1; i <= 10; i++) {
+//            LocalDate newDate = date.minusDays(i);
+//            String formattedDate = newDate.format(formatter);
+//            System.out.println("Date " + i + " day(s) ago: " + formattedDate);
+//        }
+        
+        String startDate = "2022-02-22";
+        String ednDate = "2022-03-03";
+        LocalDate startTime = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate endTime = LocalDate.parse(ednDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Period period = Period.between(startTime, endTime);
+        int days = period.getDays();
+        System.out.println(days);
+        for (int i = 1; i <= days; i++) {
+            LocalDate newDate = endTime.minusDays(i);
+            String formattedDate = newDate.format(formatter);
+            System.out.println("Date " + i + " day(s) ago: " + formattedDate);
+        }
+
+
+
     }
 
 
