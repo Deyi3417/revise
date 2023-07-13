f744b8ce386a0da5f3ee3a05633e9a8da6f48a4b
Deyi3417 <18811553417@163.com>
2023-07-13 11:41:07 +0800

    学习

--- src/main/java/deyi/com/revise/number/TestUserStream.java
+++ src/main/java/deyi/com/revise/number/TestUserStream.java
@@ -1,6 +1,7 @@
 package deyi.com.revise.number;
 
 import deyi.com.revise.list.User;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +14,30 @@ import java.util.stream.Collectors;
  */
 public class TestUserStream {
     public static void main(String[] args) {
+        String str1 = null;
+        boolean result1 = StringUtils.isBlank(str1); // true
+        boolean result5 = StringUtils.isEmpty(str1); // true
 
-        int index = 1;
-        int index1 = 1;
+        String str2 = "";
+        boolean result2 = StringUtils.isBlank(str2);
+        boolean result6 = StringUtils.isEmpty(str2); // true
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(index++);
-        }
+        String str3 = "   ";
+        boolean result3 = StringUtils.isBlank(str3);
+        boolean result7 = StringUtils.isEmpty(str3); // true
+
+        String str4 = "Hello";
+        boolean result4 = StringUtils.isBlank(str4);
+        boolean result8 = StringUtils.isEmpty(str4); // false
+        System.out.println(result1);
+        System.out.println(result5);
+        System.out.println(result2);
+        System.out.println(result6);
+        System.out.println(result3);
+        System.out.println(result7);
+        System.out.println(result4);
+        System.out.println(result8);
+
     }
     public static void test() {
         List<User> users = getUsers();
